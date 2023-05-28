21410e52fc332942375d1928ee07fec9328a962b
Depa <86026213+Dagyss@users.noreply.github.com>
2023-05-28 20:20:25 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,7 +32,7 @@ function borrar() {
     document.getElementById('inputNombre').value = '';
     document.getElementById('inputApellido').value = '';
     document.getElementById('inputEmail').value = '';
-    document.getElementById('inputCantidad').value = ' ';
+    document.getElementById('inputCantidad').value = '';
     document.getElementById('inputCategoria').value = 'Estudiante';
     document.getElementById('importeTotal').innerHTML = 'Total a Pagar: \u0024';
 }
